80de9f27c6e91e4557378a8936dbb8f08d548d49
Tomazzo Antonioli <tomazzo10@hotmail.com>
2021-12-16 17:28:53 +0100

    added home page picture and styled btn

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color: '#fff'}}>
-        This is my first react project!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./media/coffee.jpg")}
+      style={styles.image}
+    >
+      <View style={styles.container}>
+        <Text style={{ color: "#000", fontSize: 40 }}>TECH.LOG( )</Text>
+        <TouchableOpacity style={styles.btn} title="Get Started">
+          <Text>Get Started</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 70,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  btn: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    alignSelf: "center",
+    alignItems: "center",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
   },
 });
